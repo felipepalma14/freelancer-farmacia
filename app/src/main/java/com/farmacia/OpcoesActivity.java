bbbbb82a269ee6946eb4836f45ed423dfe64573a
Felipe Palma <felipepalma14@gmail.com>
2019-06-24 00:51:27 -0300

    Corrigidos alguns bugs na Classe DAO de Produto Inserido opcao para selecionar foto Criado activity de Opcoes Corrigido chamada de Banco para acessar Produtos Removido classes de conexao não utilizadas

---
+++ app/src/main/java/com/farmacia/OpcoesActivity.java
@@ -0,0 +1,25 @@
+package com.farmacia;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OpcoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opcoes);
+    }
+
+
+
+    public void listagemProdutos(View view) {
+        startActivity(new Intent(this, ListaProdutos.class));
+    }
+
+    public void listagemPorLocalizacao(View view) {
+        startActivity(new Intent(this,FarmaciaMapsActivity.class));
+    }
+}
---
+++ app/src/main/java/com/farmacia/databases/DAO/ForumDAO.java
@@ -0,0 +1,136 @@
+package com.farmacia.databases.DAO;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.farmacia.databases.DAO.interfaces.IForumDAO;
+import com.farmacia.databases.DAO.interfaces.IUsuarioDAO;
+import com.farmacia.databases.provider.DatabaseContentProvider;
+import com.farmacia.databases.schema.IForumSchema;
+import com.farmacia.databases.schema.IUsuarioSchema;
+import com.farmacia.models.Forum;
+import com.farmacia.models.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Felipe Palma on 22/06/2019.
+ */
+public class ForumDAO extends DatabaseContentProvider implements IForumSchema, IForumDAO {
+
+    private Cursor cursor;
+    private ContentValues initialValues;
+
+    private UsuarioDAO usuarioDAO;
+
+
+    public ForumDAO(SQLiteDatabase db, UsuarioDAO usuarioDAO){
+        super(db);
+        this.usuarioDAO = usuarioDAO;
+    }
+
+
+    @Override
+    public Forum getForumPorId(int id) {
+
+        final String selectionArgs[] = { String.valueOf(id) };
+        final String selection = COLUNA_ID + " = ?";
+        Forum mForum = new Forum();
+        cursor = super.query(FORUM_TABLE, FORUM_COLUNAS, selection,
+                selectionArgs, COLUNA_ID);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                mForum = cursorToEntity(cursor);
+                cursor.moveToNext();
+            }
+            cursor.close();
+        }
+
+        return mForum;
+    }
+
+    @Override
+    public List<Forum> getTodosForuns() {
+
+        ArrayList<Forum> lista = new ArrayList<>();
+        cursor = super.query(FORUM_TABLE, FORUM_COLUNAS, null,
+                null, COLUNA_ID);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                Forum item = cursorToEntity(cursor);
+                lista.add(item);
+                cursor.moveToNext();
+            }
+            cursor.close();
+        }
+
+        return lista;
+    }
+
+    @Override
+    public boolean adicionarForum(Forum forum) {
+        setContentValue(forum);
+        try {
+            return super.insert(FORUM_TABLE, getContentValue()) > 0;
+        } catch (SQLiteConstraintException ex){
+            Log.w("Database", ex.getMessage());
+            /// column email is not unique (code 19)
+            return false;
+        }
+    }
+
+
+
+
+    @Override
+    protected Forum cursorToEntity(Cursor cursor) throws CursorIndexOutOfBoundsException {
+
+        Forum mForum = new Forum();
+
+        int idIndex;
+        int topicoIndex;
+        int usuarioIndex;
+
+        if (cursor != null) {
+            if (cursor.getColumnIndex(COLUNA_ID) != -1) {
+                idIndex = cursor.getColumnIndexOrThrow(COLUNA_ID);
+                mForum.setId(cursor.getInt(idIndex));
+            }
+            if (cursor.getColumnIndex(COLUNA_TOPICO) != -1) {
+                topicoIndex = cursor.getColumnIndexOrThrow(
+                        COLUNA_TOPICO);
+                mForum.setTopico(cursor.getString(topicoIndex));
+            }
+            if (cursor.getColumnIndex(COLUNA_USUARIO_ID) != -1) {
+                usuarioIndex = cursor.getColumnIndexOrThrow(
+                        COLUNA_USUARIO_ID);
+                mForum.setUsuario(usuarioDAO.getUsuarioPorId(cursor.getInt(usuarioIndex)));
+            }
+
+        }
+        return mForum;
+    }
+
+
+    private void setContentValue(Forum forum) {
+        initialValues = new ContentValues();
+        if(forum.getId() > 0) {
+            initialValues.put(COLUNA_ID, forum.getId());
+        }
+        initialValues.put(COLUNA_TOPICO, forum.getTopico());
+        initialValues.put(COLUNA_USUARIO_ID, forum.getUsuario().getId());
+    }
+
+    private ContentValues getContentValue() {
+        return initialValues;
+    }
+
+}
